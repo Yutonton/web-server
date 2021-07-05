490192880b8ba0ceb0771608eaac78c2a23a018f
Yutonton <yutonx@gmail.com>
2021-07-05 12:51:26 +0900

    First commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require("path");
+const app = express();
+
+app.use(express.urlencoded({extended: false }));
+app.use(express.static(path.join(__dirname,"public")));
+ 
+/*
+app.get('/', function (req, res) {
+  res.send('Top')
+}); */
+
+app.post("/api/v1/quiz", function (req, res) {
+    answer = req.body.answer;
+    if(answer == "balloon"){
+       // res.send("GOOD");
+       res.redirect("/correct.html");
+    }else{
+        //res.send("BAD");
+        res.redirect("/false.html");
+
+    }
+  });
+
+app.get("/api/v1/users", function (req, res) {
+    console.log(req);
+    res.send({
+        name: "Mike",
+        age: 30,
+    });
+  });
+ 
+app.listen(3000, function(){
+    console.log("runnning");
+});
+
+console.log("endy");
